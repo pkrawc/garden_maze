ade1252f45157f980892076036c9167d8e6a6ba7
Patrick Krawczykowski <pkrawz@gmail.com>
2018-11-21 10:04:44 -0600

    intial commit

---
+++ tests/index.test.js
@@ -0,0 +1,12 @@
+import { mount } from "enzyme"
+import { expect } from "chai"
+import App from "../pages/index"
+
+describe("Garden Maze Tests", () => {
+  // const app = mount(<App />)
+  it("should create a correctly sized field", () => true)
+  it("should move down when down arrow is clicked", () => true)
+  it("should move right when right arrow is clicked", () => true)
+  it("should move left when left arrow is clicked", () => true)
+  it("should move up when up arrow is clicked", () => true)
+})
