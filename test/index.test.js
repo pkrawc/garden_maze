f0819fc240e8b81441b60b512777ac40bc0a0a07
Patrick Krawczykowski <pkrawz@gmail.com>
2018-11-22 12:50:32 -0600

    moving tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,11 @@
-import { mount } from "enzyme"
+import { shallow } from "enzyme"
 import { expect } from "chai"
 import App from "../pages/index"
 
 describe("Garden Maze Tests", () => {
-  // const app = mount(<App />)
+  // Testing is not possible with shallow. There is no renderer that can handle hooks at
+  // the moment. See https://github.com/facebook/react/issues/14091.
+  // const app = shallow(<App />)
   it("should create a correctly sized field", () => true)
   it("should move down when down arrow is clicked", () => true)
   it("should move right when right arrow is clicked", () => true)
